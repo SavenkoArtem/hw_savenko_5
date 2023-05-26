13c6eeae24b1c631204c22e9e64b2ad96607dead
ArtemUb <savenkoam@gmail.com>
2023-05-26 20:52:08 +0900

    Task 36 is Done

---
+++ task36/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-20, 20);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    Console.WriteLine();
+}
+
+int SumNotEvenNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+
+int[] array = new int[4];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"Count the number of even numbers: {SumNotEvenNumbers(array)}");
